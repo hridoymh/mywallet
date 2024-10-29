ab081033f1ac4063fe571223306fc892b9e2bece
hridoymh <mhhridoy0c@gmail.com>
2024-10-29 21:18:20 +0600

    without port

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 0;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen();
